af5624748dcd6070d273bf3cc48e128ab3b82395
oscardhc03 <60898622+oscardhc03@users.noreply.github.com>
2024-11-29 06:30:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ predictions_data = {}
 def home():
     return "Servidor Flask funcionando correctamente", 200
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     global predictions_data
 
@@ -55,5 +55,8 @@ def log_request_info():
     print(f"Solicitud recibida: {request.method} {request.url}")
 
 
+#if __name__ == '__main__':
+#   app.run(debug=True, host='0.0.0.0', port=5000)
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
